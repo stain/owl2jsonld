c58683cb58c30b89e887895ac5c702fced02e8b5
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-06-06 01:24:02 +0100

    Dependency owlapi-clj -> clj-owlapi

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"] 
-                 [owlapi-clj "1.1.0-SNAPSHOT"]
+                 [clj-owlapi "0.2.0"]
                  [cheshire "5.3.1"]
                  [org.clojure/tools.cli "0.3.1"]
                 ]
