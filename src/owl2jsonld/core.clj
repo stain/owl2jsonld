30bcd08bc1f2056c7fc74f6c5781ffc14838bd98
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-11-25 22:47:23 +0000

    whitespace..

--- src/owl2jsonld/core.clj
+++ src/owl2jsonld/core.clj
@@ -1,7 +1,7 @@
 (ns owl2jsonld.core
-  (:import 
-     (org.semanticweb.owlapi.model IRI 
-                                   OWLClass 
+  (:import
+     (org.semanticweb.owlapi.model IRI
+                                   OWLClass
                                    OWLClassExpression
                                    OWLDatatype
                                    OWLDataProperty
@@ -13,7 +13,7 @@
                                    OWLOntology
                                    OWLProperty
                                    ))
-  (:require 
+  (:require
     [owlapi.core :as owlapi]
     [clojure.set :refer [intersection union]]
     )
@@ -25,20 +25,20 @@
   (if *log*
     (binding [*out* *log*] (apply println msg))))
 
-(def ^:dynamic *options* {})      
+(def ^:dynamic *options* {})
 
-(defn inject-prefixes [options ontology] 
+(defn inject-prefixes [options ontology]
   (if (not (:inherit options)) options
     (merge options
            { :prefixes (owlapi/prefixes ontology) })))
 
-(defn prefix-for [^IRI iri] 
+(defn prefix-for [^IRI iri]
  (let [prefix (or
                 (get-in *options*  [:prefixes (.getNamespace iri)])
                 (:prefix *options*))]
    (if prefix (str prefix ":") "")))
-   
-;(if (:prefix *options*) 
+
+;(if (:prefix *options*)
 ;  (if (:prefix *options*) (str (:prefix *options*) ":") "")
 ;  )
 
@@ -55,34 +55,34 @@
   { (jsonld-name class) (named-to-jsonld class) } )
 
 (defn jsonld-type-for-property [^OWLProperty property]
-  (cond 
-    (instance? OWLObjectProperty property) 
+  (cond
+    (instance? OWLObjectProperty property)
         {"@type" "@id" }
     (instance? OWLDataProperty property)
 	    (let [ranges (owlapi/ranges-of-property property)]
-	      (cond 
+	      (cond
 	        (empty? ranges) {}
 	        (some #(instance? OWLClassExpression %) ranges) {"@type" "@id" }
-	        ; If there is only one range, which is a Datatype, we can 
-	        ; include it in JSON-LD's @type. 
-	        ;; TODO: Handle restrictions etc. on 
+	        ; If there is only one range, which is a Datatype, we can
+	        ; include it in JSON-LD's @type.
+	        ;; TODO: Handle restrictions etc. on
 	        ; ultimately uniform datatype
-	        (and (empty? (rest ranges)) (instance? OWLDatatype (first ranges))) 
+	        (and (empty? (rest ranges)) (instance? OWLDatatype (first ranges)))
 	              {"@type" (str (.getIRI (first ranges))) }
 	        :else {}))
     :else {}))
 
 (defn property-to-jsonld [^OWLProperty property]
   { (jsonld-name property)
-    (merge 
+    (merge
            (jsonld-type-for-property property)
            (named-to-jsonld property) ) } )
 
 (defn is-defined? [options ^OWLEntity entity]
   ; Must be RDFS:isDefinedby one of the specified ontologies specified
   (not (empty? (intersection
-     (:ontology-iris options)   
-     (set (map #(.getValue %) 
+     (:ontology-iris options)
+     (set (map #(.getValue %)
                (owlapi/annotations entity (:RDFSIsDefinedBy (owlapi/owl-types)))))))))
 
 (defn only-valid [options entities]
@@ -95,7 +95,7 @@
      (log "Ontology" ontology)
         (merge
             {}
-            (if (:classes options) (apply merge (map class-to-jsonld 
+            (if (:classes options) (apply merge (map class-to-jsonld
                                                      (only-valid options (owlapi/classes ontology)))))
             (if (:properties options)
              (if (:properties options)
@@ -107,10 +107,10 @@
              ))))))
 
           ;; new
-;;    FIXME: Why does this cause 
+;;    FIXME: Why does this cause
 ;; java.lang.ClassCastException: clojure.lang.PersistentArrayMap cannot be cast to java.util.Map$Entry
 ;;    ?
-;              (merge (map property-to-jsonld (only-valid options 
+;              (merge (map property-to-jsonld (only-valid options
 ;              (concat
 ;               (owlapi/annotation-properties ontology)
 ;               (owlapi/object-properties ontology)
@@ -121,24 +121,23 @@
 (defn ontology-iri [^OWLOntology ontology]
   (let [ontology-id (bean (.getOntologyID ontology))]
        ; Filter out nil
-       (keep identity 
-             (vals (select-keys ontology-id [:versionIRI :ontologyIRI]))))) 
+       (keep identity
+             (vals (select-keys ontology-id [:versionIRI :ontologyIRI])))))
 
 (defn ontology-iris [ontologies]
   (set (mapcat ontology-iri ontologies)))
 
-(defn owl2jsonld 
+(defn owl2jsonld
   [urls {:keys [all-imports no-imports inherit embed]
          :as options}]
           (owlapi/with-owl
 	  (let [ontologies (doall (map owlapi/load-ontology urls))
           all-ontologies (if all-imports (owlapi/loaded-ontologies) ontologies)
-          options (merge options 
+          options (merge options
                          { :ontology-iris
-                              (union 
+                              (union
                                 (set (map owlapi/create-iri urls))
                                 (ontology-iris ontologies))})]
-     { "@context" 
+     { "@context"
            (merge (apply merge (map (partial ontology-to-jsonld options) all-ontologies)))
       })))
-
