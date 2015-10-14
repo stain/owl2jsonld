9deef78371c2f5b15c1bc8002e7f105ef8084ebe
Victor Charpenay <vcharpenay@gmail.com>
2015-10-14 09:40:13 +0200

    (deployed a separate version of clj-owlapi for CI)

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject owl2jsonld "0.2.2-SNAPSHOT"
+(defproject owl2jsonld "0.2.3-SNAPSHOT"
   :description "Convert RDFS/OWL ontology to JSON-LD context"
   :url "https://github.com/stain/owl2jsonld"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"] 
-                 [clj-owlapi "0.3.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojars.vcharpenay/clj-owlapi "0.3.2-SNAPSHOT"]
                  [cheshire "5.3.1"]
                  [org.clojure/tools.cli "0.3.1"]
                 ]
